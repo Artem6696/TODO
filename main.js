9da898e88278c31ac4d70dbbd2fd9fec9d00a7ab
Artem <hanya66693@gmail.com>
2023-05-27 19:36:56 +0500

    delete fix id

--- main.js
+++ main.js
@@ -30,19 +30,44 @@ const showTasks = () => {
     for(const task of taskArray){
         content += `
         <div class ="task-container">
-            <div class="task-title"> ${task.title}</div>
+            <div class="task-title">[ID: ${task.id}] ${task.title}</div>
             <div class="task-status"> ${ statusTypes[task.status]}</div>
                 <div class="task-icon->
-                <div class="task-icon-container"> <img src="assets/edit.png"> </div>
-                <div class="task-icon-container"> <img src="assets/delete.png"> </div>
+                    <div class="task-icon-container">
+                    <img src="assets/edit.png"> </div>
+                <div onclick="deleteTask(${task.id})"class="task-icon-container"> 
+                    <img src="assets/delete.png"> 
                 </div>
+            </div>
         </div>    
         `
     }
     todoListContent.innerHTML = content
+}
+const getIndexById = (id) => {
+    
+    for(let i = 0; i < taskArray.length; i++){
+        const task = taskArray[i]
+        if(id == task.id) return i
+    }
+    // for(const[index,task] of Object.entries(taskArray)){
+    //     if(id == task.id) return index
+    // }
+
+    return -1
+}
+
+const deleteTask = (id) => {
+    const targetIndex = getIndexById(id)
+
+    taskArray.splice(targetIndex,1)
+
+    showTasks()
 }
 
 showTasks()
 //CRUD
 //CREATE +
 //READ  +
+//UPDATE
+//DELETE
